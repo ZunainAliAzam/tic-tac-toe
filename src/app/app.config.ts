3ec7f51316709108d9c50bffb056328b104671df
ZunainAliAzam <zunainazam456@gmail.com>
2025-02-17 13:54:14 +0500

    feat/deploy: using firebase to deploy the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PlayerSetupComponent } from './player-setup/player-setup.component';
 import { BoardComponent } from './board/board.component';
@@ -7,11 +7,17 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), // Only define routes here
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideFirebaseApp(() => initializeApp({ projectId: "tic-tac-toe-f9654", appId: "1:256374451131:web:f03ff90f86ab0123c9729e", storageBucket: "tic-tac-toe-f9654.firebasestorage.app", apiKey: "AIzaSyA6hw--4Klukd_UZ7qNzDT1c23X1Xjz1wM", authDomain: "tic-tac-toe-f9654.firebaseapp.com", messagingSenderId: "256374451131", measurementId: "G-D3HPBZ1VTV" })), provideDatabase(() => getDatabase()),
   ],
 };
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -6,18 +6,14 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <button class="w-20 h-20 text-white font-bold text-3xl" *ngIf="!value">
-      {{ value }}
-    </button>
     <button
-      class="w-20 h-20 bg-yellow-500  text-white font-bold text-3xl"
-      *ngIf="value === 'X'"
-    >
-      {{ value }}
-    </button>
-    <button
-      class="w-20 h-20 bg-lime-500 text-white font-bold text-3xl"
-      *ngIf="value === 'O'"
+      class="w-20 h-20 border-2 border-gray-700 rounded-lg text-4xl font-bold flex items-center justify-center transition-all duration-300"
+      [ngClass]="{
+        'text-gray-400 bg-gray-800': !value,
+        'bg-yellow-500 text-gray-900 shadow-lg shadow-yellow-500/50':
+          value === 'X',
+        'bg-lime-500 text-gray-900 shadow-lg shadow-lime-500/50': value === 'O'
+      }"
     >
       {{ value }}
     </button>
